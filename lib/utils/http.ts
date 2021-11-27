7929a0c53cfb8631c850fa1e082794e86e3b71c4
Kyle <KyleMit@gmail.com>
2021-11-27 11:20:53 -0500

    replace node-fetch

--- lib/utils/http.ts
+++ lib/utils/http.ts
@@ -1,12 +1,30 @@
-import fetch from "node-fetch"
+import { request, RequestOptions } from 'https'
 
-export async function fetchJson<T>(url: string): Promise<T> {
-  try {
-    const response = await fetch(url)
-    const json = await response.json() as T
-    return json
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
+
+export const fetchJson = async <T>(url: string): Promise<T> => {
+    const urlOptions = new URL(url)
+    const resp = await executeRequest(urlOptions)
+    return resp as T;
 }
+
+const executeRequest = (options: RequestOptions) => new Promise((resolve, reject) => {
+  const req = request(options, (res) => {
+      res.setEncoding('utf8');
+      let responseBody = '';
+
+      res.on('data', (chunk) => {
+          responseBody += chunk;
+      });
+
+      res.on('end', () => {
+          resolve(JSON.parse(responseBody));
+      });
+  });
+
+  req.on('error', (err) => {
+      reject(err);
+  });
+
+  req.end();
+})
+
